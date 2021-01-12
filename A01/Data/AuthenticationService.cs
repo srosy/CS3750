4d9cfe0cda6496ad9ac6822385c0769151cdc5d2
srosy <serosenvall@gmail.com>
2021-01-12 12:13:33 -0700

    initial commit

---
+++ A01/Data/AuthenticationService.cs
@@ -0,0 +1,19 @@
+﻿using A01.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A01.Data
+{
+    public class AuthenticationService
+    {
+        public Task<bool> Authenticate(AuthModel model)
+        {
+            var authenticated = false;
+            //TODO: determine if login is a success
+
+            return Task.Run(() => 1 == 0);
+        }
+    }
+}
