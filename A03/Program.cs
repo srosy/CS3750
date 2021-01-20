05852d02efc6f1d01fc2ad33a701467bbb0eae25
srosy <serosenvall@gmail.com>
2021-01-20 16:45:30 -0700

    A03 done

---
+++ A03/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace A03
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Enter a stock code: ");
+            var code = Console.ReadLine().ToUpper();
+            var response = await ExecuteCallAsync(code);
+            if (string.IsNullOrEmpty(response))
+                Console.WriteLine("Invalid code.");
+            else
+                Console.WriteLine($"Response: {response}");
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Executes an async Get to the AlphaAdvantage API. Returns response string provided passed code exists.
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        static async Task<string> ExecuteCallAsync(string code)
+        {
+            try
+            {
+                var client = new HttpClient();
+                var uri = $"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=$&apikey=3O1E938UHPASK7HX".Replace("$", code);
+                string responseBody = await client.GetStringAsync(uri);
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+            return string.Empty;
+        }
+    }
+}
